db57fda4ea68967d475ac224314dcfaaee202724
Yoshihiro WADA <1365196t@stu.kobe-u.ac.jp>
2016-02-17 00:30:54 +0900

    Rename Uzuki to Adxl345 and separate initialize and reading

--- Uzuki/app/src/main/java/com/fabworks/macnica/uzuki/sensor/Adxl345.java
+++ Uzuki/app/src/main/java/com/fabworks/macnica/uzuki/sensor/Adxl345.java
@@ -1,4 +1,4 @@
-package com.fabworks.macnica.uzuki.shield;
+package com.fabworks.macnica.uzuki.sensor;
 
 import android.bluetooth.BluetoothGattCharacteristic;
 
@@ -11,19 +11,17 @@ import info.izumin.android.bletia.BletiaException;
 /**
  * Created by e10dokup on 2016/02/16
  **/
-public class Uzuki {
-    private static final String TAG = Uzuki.class.getSimpleName();
-    private final Uzuki self = this;
+public class Adxl345 {
+    private static final String TAG = Adxl345.class.getSimpleName();
+    private final Adxl345 self = this;
 
     public static final int ACC_SENSOR_ADDRESS = 0x1D;
-    
-    public static Promise<byte[], BletiaException, Void> readAccelerometer(KonashiManager manager) {
+
+    public static Promise<BluetoothGattCharacteristic, BletiaException, Void> initialize(KonashiManager manager) {
         byte[] data1 = {0x31, 0x0B};
         byte[] data2 = {0x2D, 0x08};
         byte[] data3 = {0x24, 0x20};
         byte[] data4 = {0x27, (byte)0xF0};
-        byte[] data5 = {0x32};
-
 
         return manager.<BluetoothGattCharacteristic>i2cWrite(data1.length, data1, (byte) ACC_SENSOR_ADDRESS)
                 .then(manager.<BluetoothGattCharacteristic>i2cStopConditionPipe())
@@ -34,10 +32,13 @@ public class Uzuki {
                 .then(manager.<BluetoothGattCharacteristic>i2cWritePipe(data3.length, data3, (byte) ACC_SENSOR_ADDRESS))
                 .then(manager.<BluetoothGattCharacteristic>i2cStopConditionPipe())
                 .then(manager.<BluetoothGattCharacteristic>i2cStartConditionPipe())
-                .then(manager.<BluetoothGattCharacteristic>i2cWritePipe(data4.length, data4, (byte) ACC_SENSOR_ADDRESS))
-                .then(manager.<BluetoothGattCharacteristic>i2cStopConditionPipe())
-                .then(manager.<BluetoothGattCharacteristic>i2cStartConditionPipe())
-                .then(manager.<BluetoothGattCharacteristic>i2cWritePipe(data5.length, data5, (byte) ACC_SENSOR_ADDRESS))
+                .then(manager.<BluetoothGattCharacteristic>i2cWritePipe(data4.length, data4, (byte) ACC_SENSOR_ADDRESS));
+    }
+    
+    public static Promise<byte[], BletiaException, Void> readAccelerometer(KonashiManager manager) {
+        byte[] data = {0x32};
+
+        return manager.<BluetoothGattCharacteristic>i2cWrite(data.length, data, (byte) ACC_SENSOR_ADDRESS)
                 .then(manager.<BluetoothGattCharacteristic>i2cStopConditionPipe())
                 .then(manager.<BluetoothGattCharacteristic>i2cStartConditionPipe())
                 .then(manager.<BluetoothGattCharacteristic>i2cReadPipe(6, (byte) ACC_SENSOR_ADDRESS));
